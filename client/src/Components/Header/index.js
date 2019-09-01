0533c74fe000e45d1223a9c1516c6313dd93d266
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-01 15:18:18 +0300

    rename the file

---
+++ client/src/Components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./style.css"
+const Header = () => (
+  <div className="header1 ">
+    <div className="logo" />
+    <a className="switch" href="www.google.com">
+      Continue as a pharmacist
+    </a>
+  </div>
+)
+
+export default Header
