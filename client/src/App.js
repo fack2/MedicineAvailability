9cd768671c7fa5233bb701a91c6fb8fcf0a037ec
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-01 14:27:10 +0300

    creating parent for header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import React from "react"
-import "./App.css"
+import React, { Component } from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
-function App() {
-  return (
-    <div>
-      <p>hello</p>
-    </div>
-  )
+import Header from "./Components/Header/Header.js"
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <React.Fragment>
+          <Route exact path="/" component={Header} />
+        </React.Fragment>
+      </Router>
+    )
+  }
 }
 
 export default App
