80e79a08989d4dc39392fcd7cf3ef5cd7b0af6db
yaqootturman <yaqoot.turman@gmail.com>
2019-09-02 10:19:01 +0300

    homePage which contain search

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Home from "./components/Home/index"
+import Result from "./components/Results/index"
+
 import "./App.css"
 
 class App extends Component {
--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,20 +1,57 @@
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
-import "./home.css"
+import "./search.css"
+import axios from "axios"
 
-function Home() {
-  return (
-    <div>
-      <span>
-        The app is an easy way to search for the medicine you need in your next
-        door pharmacy.
-      </span>
-      <input className="searchBar" type="text" name="firstname" />
-      <button class="btn btn-secondary" type="button">
-        <i class="fa fa-search"></i>
-      </button>
-    </div>
-  )
+class Search extends Component {
+  state = {
+    pharmaciesResult: "",
+    medicineName: ""
+  }
+
+  changInput = event => {
+    const { target } = event
+    const { value } = target
+    this.setState({ medicineName: value })
+  }
+
+  handlesearch = () => {
+    const { pharmaciesResult, medicineName } = this.state
+    //This should handle in backend
+    axios.get(`/api/medicine/${medicineName}`).then(({ data }) => {
+      this.setState({
+        pharmaciesResult: data
+      })
+    })
+    // const { history } = this.props
+    // history.push("/results")
+  }
+
+  render() {
+    const { pharmaciesResult, medicineName } = this.state
+    if (pharmaciesResult) {
+      return (
+        <p>this should be a results pharmacies which get it by api calling </p>
+      )
+    } else {
+      return (
+        <div>
+          <span>
+            The app is an easy way to search for the medicine you need in your
+            next door pharmacy.
+          </span>
+          <input
+            className="searchBar"
+            value={medicineName}
+            onChange={this.changInput}
+            type="text"
+            name="firstname"
+          />
+          <button onClick={this.handlesearch}>search</button>
+        </div>
+      )
+    }
+  }
 }
 
-export default Home
+export default Search
