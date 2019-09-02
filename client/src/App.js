02e435595708bc8ac19bb94bb5b21bbd826e4285
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-02 09:15:34 +0300

    make app function

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import HomePage from "./Components/HomePage"
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <React.Fragment>
-          <Route exact path="/" component={HomePage} />
-        </React.Fragment>
-      </Router>
-    )
-  }
-}
+const App = () => (
+  <Router>
+    <React.Fragment>
+      <Route exact path="/" component={HomePage} />
+    </React.Fragment>
+  </Router>
+)
 
 export default App
