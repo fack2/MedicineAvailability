b9106e2f38aefacc1ee213c232e3b31768e02132
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-02 10:21:53 +0300

    create path for results page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react"
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import HomePage from "./Components/HomePage"
+import CustomerPharmacyInfo from "./Components/CustomerPharmacyInfo"
 
 const App = () => (
   <Router>
     <React.Fragment>
       <Route exact path="/" component={HomePage} />
+      <Route path="/CustomerPharmacyInfo" component={CustomerPharmacyInfo} />
     </React.Fragment>
   </Router>
 )
