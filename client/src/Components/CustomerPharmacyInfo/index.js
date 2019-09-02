314ced2acf043d45857fc7e378e5a3843bb1752f
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-02 10:20:56 +0300

    make component for results

---
+++ client/src/Components/CustomerPharmacyInfo/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+import "./style.css"
+
+class CustomerPharmacyInfo extends Component {
+  render() {
+    return (
+      <div className="pharmacyInfo">
+        <h3 className="pharmacyName">Pharmacy Name</h3>
+        <h3 className="price">11 ILS</h3>
+      </div>
+    )
+  }
+}
+export default CustomerPharmacyInfo
