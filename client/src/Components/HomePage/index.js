bcdf3a828c76b8de6c64f1981698952c1e2153c4
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-03 16:08:48 +0300

    resolved problem in result page

--- client/src/Components/HomePage/index.js
+++ client/src/Components/HomePage/index.js
@@ -1,46 +1,44 @@
-import React, { Component } from 'react'
-import Header from '../Header'
-import Search from '../Search'
-import axios from 'axios'
-import PharmacyHome from '../PharmacyHome';
+import React, { Component } from "react"
+import Header from "../Header"
+import Search from "../Search"
+import axios from "axios"
+import PharmacyHome from "../PharmacyHome"
 
 class HomePage extends Component {
-	state = {
-		pharmaciesResult: "",
-		medicineName: ""
-	}
+  state = {
+    pharmaciesResult: "",
+    medname: ""
+  }
 
-	changeInput = ({ target: { value, name } }) => {
-		this.setState({ [name]: value })
-	}
+  changeInput = ({ target: { value, name } }) => {
+    this.setState({ [name]: value })
+  }
 
-	searchHandler = () => {
-		const { pharmaciesResult, medname: medname } = this.state
-		const { history } = this.props
+  searchHandler = () => {
+    const { pharmaciesResult, medname } = this.state
+    const { history } = this.props
 
-		// This should handle in backend
-		axios.get(`/api/medicine/${medname}`).then(({ data }) => {
-			console.log(data)
-			this.setState({
-				pharmaciesResult: data
-			})
-			history.push({ pathname: '/results', data: pharmaciesResult })
-			console.log(pharmaciesResult)
-		})
-	}
+    // This should handle in backend
+    axios.get(`/api/medicine/${medname}`).then(({ data }) => {
+      this.setState({
+        pharmaciesResult: data
+      })
+      history.push({ pathname: "/results", data })
+    })
+  }
 
-	render() {
-		return (
-			<>
-				<Header />
-				<Search
-					handleSearch={this.searchHandler}
-					changInput={this.changeInput}
-					medname={this.state.medname}
-					pharmaciesResult={this.state.pharmaciesResult}
-				/>
-			</>
-		)
-	}
+  render() {
+    return (
+      <>
+        <Header />
+        <Search
+          handleSearch={this.searchHandler}
+          changInput={this.changeInput}
+          medname={this.state.medname}
+          pharmaciesResult={this.state.pharmaciesResult}
+        />
+      </>
+    )
+  }
 }
 export default HomePage
