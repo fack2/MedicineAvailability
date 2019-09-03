f0afb522274d7eb31eca58e943cbc859d35c2f93
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-03 15:08:48 +0300

    resolved commit

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const search = require('./search')
 const { auth } = require('../middlewares/auth')
-const checkAuth = require('./checkAuth')
+const { checkAuth } = require('./checkAuth')
 
 router.get('/api/medicine/:medname', search.get)
 router.get('/check-auth', auth, checkAuth)
