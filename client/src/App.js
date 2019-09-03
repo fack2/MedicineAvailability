55e8bfe3894efc4ca1857bce563c4ad83b099603
sara219 <whitenoise6014@gmail.com>
2019-09-03 11:39:17 +0300

    add the pharmacy home to the app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,15 @@ import React, { Component } from 'react'
 // eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Results from './Components/Results'
-
 import HomePage from './Components/HomePage'
+import PharmacyHome from './Components/PharmacyHome'
 
 const App = () => (
   <Router>
     <React.Fragment>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/results" component={Results} />
+      <Route exact phath='/pharmacy' component={PharmacyHome} />
     </React.Fragment>
   </Router>
 )
