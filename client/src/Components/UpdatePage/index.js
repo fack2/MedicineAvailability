8df20a541ead9ca7ae15f2607a7657add69b752e
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-03 16:00:19 +0300

    make page as a container for update form

---
+++ client/src/Components/UpdatePage/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import UpdateForm from '../UpdateForm'
+class UpdatePage extends Component {
+  render () {
+    return (
+      <>
+        <UpdateForm />
+      </>
+    )
+  }
+}
+export default UpdatePage
