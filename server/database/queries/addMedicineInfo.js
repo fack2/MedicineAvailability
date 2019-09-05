f5ae5e8ecf877bccc7a5e1f907472a080c7ecb03
sara219 <whitenoise6014@gmail.com>
2019-09-05 15:46:00 +0300

    remove the console.log

--- server/database/queries/addMedicineInfo.js
+++ server/database/queries/addMedicineInfo.js
@@ -1,7 +1,6 @@
 const dbConnection = require('./../db_connection')
 
 const addMedicineInfo = (medicine, description, pharmacyID) => {
-  console.log(pharmacyID, 'hiiiiiiiiiiiii')
   return dbConnection
     .query('with inserted(id) as (insert into medicine(name, company, prescription, description) values ($1, $2, $3, $4) returning medicineID) insert into pharmacy_to_medicine (medicineID, pharmacyID, price) values ( (select id from inserted) ,$5 , $6)', [medicine.medName, medicine.medCompany, medicine.prescription, description, pharmacyID, medicine.medPrice])
     .then(res => res.rows[0])
