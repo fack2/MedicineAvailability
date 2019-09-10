5ef4eeee6e9d7271a192b500ecefe199f9b58084
DuaaH <dodohalaiqa.1@gmail.com>
2019-09-10 16:39:54 +0300

    destructing props

--- client/src/Components/Header/index.js
+++ client/src/Components/Header/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import './style.css'
 import { Link } from 'react-router-dom'
 
-const Header = props => (
+const Header = ({ home }) => (
   <div className="header1">
     <div className="logo" />
-    <Link className="switch" to={props.home ? '/login' : '/'}>
-      {props.home ? 'Continue as a pharmacist' : 'Switch to normal user'}
+    <Link className="switch" to={home ? '/login' : '/'}>
+      {home ? 'Continue as a pharmacist' : 'Switch to normal user'}
     </Link>
   </div>
 )
