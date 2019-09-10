d1ff5a0f68b8261aa50b202700008a1d31fa4d7e
95Rawan <youlostme17@hotmail.com>
2019-09-10 11:16:01 +0300

    apdated branch

--- client/src/Components/Results/index.js
+++ client/src/Components/Results/index.js
@@ -9,26 +9,26 @@ class Results extends Component {
     const info = result
 
     return (
-      <>
-        <NavBar login={false} {...this.props} />
-        {info.map((item, i) => {
-          return (
-            <CustomerPharmacyInfo
-              item={item}
-              pharmacyname={item.pharmacyname}
-              price={item.price}
-              history={this.props.history}
-              key={i}
-            />
-          )
-        })}
-        <UserSearchResults
-          img={info[0].img}
-          description={info[0].description}
-          medicinename={info[0].medicinename}
-
-        />
-      </>
+			<>
+				<NavBar login={false} {...this.props} />
+				{info.map((item, i) => {
+				  return (
+				    <CustomerPharmacyInfo
+				      item={item}
+				      pharmacyname={item.pharmacyname}
+				      price={item.price}
+				      history={this.props.history}
+				      key={i}
+				    />
+				  )
+				})}
+				<UserSearchResults
+				  img={info[0].img}
+				  description={info[0].description}
+				  medicinename={info[0].medicinename}
+				  prescription={info[0].prescription}
+				/>
+			</>
     )
   }
 }
