9cb09952428a2389f92b23a81c092b281df58db1
yaqootturman <yaqoot.turman@gmail.com>
2019-09-11 16:06:29 +0300

    styling for update form

--- client/src/Components/UserSearchResults/index.js
+++ client/src/Components/UserSearchResults/index.js
@@ -1,21 +1,11 @@
 import React from 'react'
 import './style.css'
 
-const UserSearchResults = ({
-  img,
-  description,
-  medicinename,
-  prescription
-}) => (
+const UserSearchResults = ({ img, description, medicinename }) => (
   <div className="medicineInformation">
     <img src={img} alt="medicinImage" className="medicineImage" />
     <h3 className="nameMed">{medicinename}</h3>
     <p className="description">{description}</p>
-    {prescription === true ? (
-      <p className="pre">prescription: YES</p>
-    ) : (
-      <p className="pre">prescription: NO </p>
-    )}
   </div>
 )
 
