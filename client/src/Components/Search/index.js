04991ebe2faa559084ee4fce6438d51da82ea397
95Rawan <youlostme17@hotmail.com>
2019-09-12 12:55:16 +0300

    Solved loading problem

--- client/src/Components/Search/index.js
+++ client/src/Components/Search/index.js
@@ -1,117 +1,135 @@
-import React, { Component } from "react"
-import "./style.css"
-import axios from "axios"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import React, { Component } from 'react'
+import './style.css'
+import axios from 'axios'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import ClipLoader from 'react-spinners/ClipLoader'
 
 class Search extends Component {
-  state = {
-    pharmaciesResult: "",
-    medname: "",
-    location: "",
-    pharmacy: "",
-    searchButtonClicked: false
-  }
+	state = {
+		pharmaciesResult: '',
+		medname: '',
+		location: '',
+		pharmacy: '',
+		searchButtonClicked: false,
+		loading: false
+	}
 
-  changeInput = ({ target: { value, name } }) => {
-    this.setState({ [name]: value, searchButtonClicked: false })
-  }
+	changeInput = ({ target: { value, name } }) => {
+		this.setState({ [name]: value, searchButtonClicked: false })
+	}
 
-  handleSearch = () => {
-    const { medname, location, pharmacy } = this.state
-    const { history } = this.props
-    this.setState({ searchButtonClicked: true })
-    if (!medname) {
-      return
-    }
-    axios.get(`/api/medicine/${medname}`).then(({ data }) => {
-      let result = ""
-      if (location && pharmacy) {
-        result = data.data.filter(
-          ele => ele.location == location && ele.pharmacyname == pharmacy
-        )
-      } else if (location) {
-        result = data.data.filter(ele => ele.location === location)
-      } else if (pharmacy) {
-        result = data.data.filter(ele => ele.pharmacy === pharmacy)
-      } else {
-        result = data.data
-      }
+	handleSearch = () => {
+		const { medname, location, pharmacy } = this.state
+		const { history } = this.props
+		this.setState({ searchButtonClicked: true })
 
-      this.setState({ pharmaciesResult: result })
+		if (!medname) {
+			return
+		}
 
-      if (result.length !== 0) {
-        sessionStorage.setItem("results", JSON.stringify(result))
-        history.push({
-          pathname: "/results"
-        })
-      }
-    })
-  }
+		this.setState({ loading: true })
 
-  searchByLocation = ({ target: { value } }) => {
-    this.setState({ location: value })
-  }
+		axios.get(`/api/medicine/${medname}`).then(({ data }) => {
+			let result = ''
+			if (location && pharmacy) {
+				result = data.data.filter(
+					ele => ele.location == location && ele.pharmacyname == pharmacy
+				)
+			} else if (location) {
+				result = data.data.filter(ele => ele.location === location)
+			} else if (pharmacy) {
+				result = data.data.filter(ele => ele.pharmacy === pharmacy)
+			} else {
+				result = data.data
+			}
+			this.setState({ loading: false })
+			if (result.length !== 0) {
+				this.setState({ pharmaciesResult: result, loading: false })
+				sessionStorage.setItem('results', JSON.stringify(result))
+				history.push({
+					pathname: '/results'
+				})
+			}
+		})
+	}
 
-  searchByPharmacy = ({ target: { value } }) => {
-    this.setState({ pharmacy: value })
-  }
+	searchByLocation = ({ target: { value } }) => {
+		this.setState({ location: value })
+	}
 
-  render() {
-    const {
-      pharmaciesResult,
-      medname,
-      location,
-      pharmacy,
-      searchButtonClicked
-    } = this.state
+	searchByPharmacy = ({ target: { value } }) => {
+		this.setState({ pharmacy: value })
+	}
 
-    return (
-      <div>
-        <p className="span">
-          The app is an easy way to search for the medicine you need in your
-          next door pharmacy.
-        </p>
-        <input
-          className="searchBar"
-          value={medname}
-          onChange={this.changeInput}
-          type="text"
-          name="medname"
-        />
-        <button onClick={this.handleSearch}>
-          <FontAwesomeIcon icon="search" />
-        </button>
-        <select
-          className="By-Location"
-          value={location}
-          name="By location"
-          onChange={this.searchByLocation}
-        >
-          <option value="">By Location</option>
-          <option value="Ras El-Jora">Ras El-Jora</option>
-          <option value="Al-Manara square">Al-Manara square</option>
-          <option value="Ein Sara">Ein Sara</option>
-        </select>
-        <select
-          value={pharmacy}
-          name="By pharmacy"
-          onChange={this.searchByPharmacy}
-          className="By-Pharmacy"
-        >
-          <option value="">By Pharmacy</option>
-          <option value="Al-Eman Pharmacy">Al-Eman Pharmacy</option>
-          <option value="Al-Jazera">Al-Jazera</option>
-        </select>
-        {!medname && searchButtonClicked && (
-          <p className="noMedicineName">Please enter a medicine name</p>
-        )}
+	render() {
+		const {
+			pharmaciesResult,
+			medname,
+			location,
+			pharmacy,
+			searchButtonClicked,
+			loading
+		} = this.state
 
-        {!pharmaciesResult ||
-          (searchButtonClicked && medname && (
-            <p className="noResult">There is no result</p>
-          ))}
-      </div >
-    )
-  }
+		return (
+			<div>
+				<p className="span">
+					The app is an easy way to search for the medicine you need in your
+					next door pharmacy.
+				</p>
+				<input
+					className="searchBar"
+					value={medname}
+					onChange={this.changeInput}
+					type="text"
+					name="medname"
+				/>
+				<button onClick={this.handleSearch}>
+					<FontAwesomeIcon icon="search" />
+				</button>
+				<select
+					className="By-Location"
+					value={location}
+					name="By location"
+					onChange={this.searchByLocation}
+				>
+					<option value="">By Location</option>
+					<option value="Ras El-Jora">Ras El-Jora</option>
+					<option value="Al-Manara square">Al-Manara square</option>
+					<option value="Ein Sara">Ein Sara</option>
+				</select>
+				<select
+					value={pharmacy}
+					name="By pharmacy"
+					onChange={this.searchByPharmacy}
+					className="By-Pharmacy"
+				>
+					<option value="">By Pharmacy</option>
+					<option value="Al-Eman Pharmacy">Al-Eman Pharmacy</option>
+					<option value="Al-Jazera">Al-Jazera</option>
+				</select>
+				{!medname && searchButtonClicked ? (
+					<p className="noMedicineName">Please enter a medicine name</p>
+				) : (
+					<p></p>
+				)}
+
+				{searchButtonClicked && medname && loading && (
+					<div className="loadingSearchP ">
+						<ClipLoader
+							className="loadingSearch"
+							sizeUnit={'px'}
+							size={35}
+							color={'#123abc'}
+						/>
+					</div>
+				)}
+
+				{searchButtonClicked && medname && !loading && !pharmaciesResult && (
+					<p className="noResult">There is no result</p>
+				)}
+			</div>
+		)
+	}
 }
 export default Search
