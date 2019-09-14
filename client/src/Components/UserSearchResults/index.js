43ab31606897111b823f20c7a426317c6be04d31
95Rawan <youlostme17@hotmail.com>
2019-09-14 14:11:15 +0300

    Used read more-less component and styled it

--- client/src/Components/UserSearchResults/index.js
+++ client/src/Components/UserSearchResults/index.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import './style.css'
+import ReadMoreAndLess from 'react-read-more-less'
 
-const UserSearchResults = ({
-  img,
-  description,
-  medicinename,
-  prescription
-}) => (
+const UserSearchResults = ({ img, description, medicinename }) => (
   <div className="medicineInformationD">
     <img src={img} alt="medicinImage" className="medicineImageD" />
     <h3 className="nameMedD">{medicinename}</h3>
-    <p className="descriptionD">{description}</p>
+    <p className="descriptionD">
+      <ReadMoreAndLess
+        className="description"
+        charLimit={200}
+        readMoreText="Read more"
+        readLessText="Read less"
+      >
+        {description}
+      </ReadMoreAndLess>
+    </p>
   </div>
 )
 
